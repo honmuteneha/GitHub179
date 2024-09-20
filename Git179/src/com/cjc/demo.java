6beb9a9794bde89fd2ef70889470ff8d264d38c2
Hp <Hp@DESKTOP-T791GB6>
2024-09-20 15:30:36 +0530

    changes

--- Git179/src/com/cjc/demo.java
+++ Git179/src/com/cjc/demo.java
@@ -1,6 +1,10 @@
 package com.cjc;
 
 public class demo {
+	public void m1()
+	{
+		System.out.println("m1-method call");
+	}
 	public static void main(String[] args) {
 		System.out.println("git project");
 	}
